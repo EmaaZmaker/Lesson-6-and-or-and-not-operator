2b1a2b36287a412b873d7980f3e9f1a992ff2906
EmaaZmaker <emaazbinammar@gmail.com>
2025-03-01 11:12:07 +0400

    activity1

---
+++ activity1.py
@@ -0,0 +1,14 @@
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("this is the or operator")
+else:
+    print("both the values are false")
+
+if a>0 and c>0:
+    print("this is the and operator")
+else:
+    print("one value is false")
+    
+    
